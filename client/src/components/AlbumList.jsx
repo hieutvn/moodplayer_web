ce6ec4948b45516ccaa8c28cabd06a837f4b9c8a
Hieu <hieutavan13@gmail.com>
2025-10-05 12:20:19 +0200

    AlbumList data fetching fix

--- client/src/components/AlbumList.jsx
+++ client/src/components/AlbumList.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState, useMemo } from 'react';
+import { useEffect, useContext, useState, useMemo, useCallback } from 'react';
 import styles from '../assets/styles/albumlist.module.css';
 
 import { AlbumContext, TokenContext } from './App';
@@ -7,7 +7,6 @@ export default function AlbumList() {
 
     const [currentAlbum] = useContext(AlbumContext);
     const [accessTokenState] = useContext(TokenContext);
-
 
     const [songs, setSongs] = useState([]);
     const [albumList, setAlbumList] = useState([]);
@@ -18,7 +17,7 @@ export default function AlbumList() {
 
         const albumID = currentAlbum.uri.slice(14); // EXCLUDES "spotify:album:"
 
-        async function requestAlbum() {
+        const requestAlbum = async () => {
 
             try {
                 const fetchAlbum = await fetch(`https://api.spotify.com/v1/albums/${albumID}`, {
@@ -29,63 +28,52 @@ export default function AlbumList() {
                     })
                 });
 
-                const data = await fetchAlbum.json();
-                //console.log(data);
+                const album = await fetchAlbum.json();
 
+                //return album.tracks.items; // RETURNS ARRAY
+
+                album.tracks.items.map((item) => {
+
+                    setAlbumList(prev => [...prev, {
+                        artist: item.artists.map((artist) => artist.name).join(", "),
+                        name: item.name,
+                        duration: (((item.duration_ms / 1000) / 60).toFixed(2))
+                    }]);
+                })
             }
             catch (error) { console.error(error) }
             if (!accessTokenState) { throw new Error('No Token loaded.') }
         }
-        requestAlbum();
+        requestAlbum()
+            .catch(console.error);
+
     }, [currentAlbum]);
 
-    useMemo(() => {
 
-        if (!currentAlbum) return;
-
-        const albumID = currentAlbum.uri.slice(14); // EXCLUDES "spotify:album:"
-
-        async function requestAlbum() {
-
-            try {
-                const fetchAlbum = await fetch(`https://api.spotify.com/v1/albums/${albumID}`, {
-
-                    method: 'GET',
-                    headers: new Headers({
-                        Authorization: "Bearer " + accessTokenState,
-                    })
-                });
-
-                const data = await fetchAlbum.json();
-                console.log("Token in AlbumList", data); // >>TBD.<<
-
-            }
-            catch (error) { console.error(error) }
-            if (!accessTokenState) { throw new Error('No Token loaded.') }
-        }
-        requestAlbum();
-    }, [currentAlbum]);
-
+    if (albumList) console.log(albumList[0])
 
     if (!accessTokenState) { return <h1>Loading..</h1> }
-    else if (accessTokenState) {
+    else if (albumList) {
         return (
             <div className={styles.albumlist}>
                 <div className={styles.artist_container}>
                     <img className={styles.artist_img} src="#" alt="Artist image" />
-                </div>
-                <div className={styles.list_container}>
-                    <div className={styles.song_container}>
+                </div >
+                <ul className={styles.list_container}>
+                    {
+                        albumList.map((item, index) => (
+                            <li key={index} className={styles.song_container}>
+                                <div className={styles.song_info}>
+                                    <div className={styles.song_title}>{item.name}</div>
+                                    <div className={styles.song_artist}>{item.artist}</div>
+                                </div>
 
-                        <div className={styles.song_info}>
-                            <div className={styles.song_title}>Annihilate</div>
-                            <div className={styles.song_artist}>Swae Lee, Lil Wayne and Offset</div>
-                        </div>
-
-                        <div className={styles.song_duration}>03:51</div>
-                    </div>
-                </div>
-            </div>
+                                <div className={styles.song_duration}>{item.duration.replace(".", ":")}</div>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div >
         )
     }
 }
